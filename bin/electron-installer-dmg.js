efa5acecc5ffb8bce7bb8b2b112952e39b96d2f2
Javier Cerro Lozano <javier.cerrolozano@intuitivesoft.es>
2016-08-17 22:38:34 +0200

    Changes fix issues

--- bin/electron-installer-dmg.js
+++ bin/electron-installer-dmg.js
@@ -5,11 +5,11 @@ var fs = require('fs');
 /*eslint no-sync:0*/
 var usage = fs.readFileSync(path.resolve(__dirname, '../usage.txt')).toString();
 var args = require('minimist')(process.argv.slice(2), {
-  boolean: ['debug', 'overwrite']
+    boolean: ['debug', 'overwrite']
 });
 
 if (args.debug) {
-  process.env.DEBUG = 'electron-installer-dmg';
+    process.env.DEBUG = 'electron-installer-dmg';
 }
 var createDMG = require('../');
 var pkg = require('../package.json');
@@ -18,19 +18,19 @@ args.appPath = args._[0];
 args.name = args._[1];
 
 if (args.help || args.h || !args.appPath || !args.name) {
-  console.error(usage);
-  process.exit(1);
+    console.error(usage);
+    process.exit(1);
 }
 if (args.version) {
-  console.error(pkg.version);
-  process.exit(1);
+    console.error(pkg.version);
+    process.exit(1);
 }
 
-createDMG(args, function(err) {
-  if (err) {
-    console.error(err);
-    process.exit(1);
-    return;
-  }
-  console.error('Wrote DMG to:\n' + args.dmgPath);
+createDMG(args, function (err) {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+        return;
+    }
+    console.error('Wrote DMG to:\n' + args.dmgPath);
 });
